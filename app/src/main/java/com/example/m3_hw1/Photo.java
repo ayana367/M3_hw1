18aeb0abee2067b35b185188c8d5b49e513661f4
user <ayanakim258@gmail.com>
2022-11-11 19:10:14 +0600

    Initial commit

---
+++ app/src/main/java/com/example/m3_hw1/Photo.java
@@ -0,0 +1,25 @@
+package com.example.m3_hw1;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Photo extends AppCompatActivity {
+    TextView x;
+    TextView x1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.photo);
+        x = findViewById(R.id.x);
+        x1 = findViewById(R.id.x1);
+        String s = x1.getText().toString();
+        Intent intent = getIntent();
+        String dev = s + intent.getStringExtra("dev");
+        x.setText(dev);
+
+    }
+}
